f8fa9daf42f1b57ea487a9b6579069cc5fb62abe
aryeh <warmflasha@gmail.com>
2016-08-20 11:09:38 -0700

    First Homework

---
+++ HW1/HW1.m
@@ -0,0 +1,23 @@
+%% Run these two sections. explain why one gives a sensible result and the other doesn't
+% extra credit: can you explain the value of the variable b? 
+x = 1; 
+y = x + x
+%%
+a = '1';
+b = a+a
+%% what is the difference between these two arrays
+x2 = [x x]
+
+a2 = [a a]
+%% Fill in the blank space in this section with code that will add 
+% the two numbers regardless of variable type. Hint see the matlab
+% functions ischar, isnumeric, and str2num. 
+
+%your code should work no matter which of these lines is commented. 
+x = 3; y = 5; % integers
+%x = '3'; y= '5';
+
+%your code goes here
+
+%output your answer
+%%
